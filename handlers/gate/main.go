36ac07e4adf78b230ee824b7e5cc1c651becee24
Laszlo Pusok <laszlo.pusok@bitrise.io>
2020-02-11 09:51:28 +0100

    Print rebuild command on failure

--- handlers/gate/main.go
+++ handlers/gate/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -24,6 +25,38 @@ func failf(format string, v ...interface{}) {
 	os.Exit(1)
 }
 
+func rebuildAPICall() (string, error) {
+	envVars := [...]string{
+		"workflow_id",
+		"commit_hash",
+		"commit_message",
+		"branch",
+		"branch_repo_owner",
+		"branch_dest",
+		"branch_dest_repo_owner",
+		"pull_request_id",
+		"pull_request_repository_url",
+		"pull_request_merge_branch",
+		"pull_request_head_branch",
+		"pull_request_author",
+		"diff_url",
+		"commit_path",
+	}
+	envVarToContents := map[string]string{"BYPASS_GATE": "true"}
+	for _, envName := range envVars {
+		envVarToContents[envName] = os.Getenv(envName)
+	}
+
+	buildParams, err := json.Marshal(envVarToContents)
+	if err != nil {
+		return "", fmt.Errorf("rebuildAPICall: failed to marshal build params, envVars: %s, %v", envVars, err)
+	}
+
+	return fmt.Sprintf(`$ curl https://app.bitrise.io/app/a0bac497f75e1490/build/start.json --data '{"hook_info":{"type":"bitrise","build_trigger_token":" < insert_build_trigger_token > "},
+"build_params": %s,
+"triggered_by":"curl"}'`, string(buildParams)), nil
+}
+
 func main() {
 	var c Config
 	if err := stepconf.Parse(&c); err != nil {
@@ -39,9 +72,15 @@ func main() {
 		failf("Failed to get changed files for branch: %s: %s", baseBranch, err)
 	}
 
-	for _, file := range changedFiles {
-		if strings.HasSuffix(file, "step-info.yml") && !c.BypassGate {
-			//fail here
+	if !c.BypassGate {
+		for _, file := range changedFiles {
+			if strings.HasSuffix(file, "step-info.yml") {
+				rebuildCall, err := rebuildAPICall()
+				if err != nil {
+					log.Warnf("%s", err)
+				}
+				failf("step-info.yml has changed, please re-check changes and rebuild with BYPASS_GATE env to true to pass this check, using: " + rebuildCall)
+			}
 		}
 	}
 
