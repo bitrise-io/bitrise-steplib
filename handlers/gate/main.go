d98ed2cc4df8bdd649d8dfb9a9ab4b1f1e3b8c0b
Laszlo Pusok <laszlo.pusok@bitrise.io>
2020-02-11 11:31:10 +0100

    Using buildParams

--- handlers/gate/main.go
+++ handlers/gate/main.go
@@ -20,37 +20,63 @@ type Config struct {
 	BypassGate bool `env:"BYPASS_GATE"`
 }
 
+// BuildParamsModel ...
+type BuildParamsModel struct {
+	Branch                   string                     `json:"branch"`
+	Tag                      string                     `json:"tag"`
+	CommitHash               string                     `json:"commit_hash"`
+	CommitMessage            string                     `json:"commit_message"`
+	WorkflowID               string                     `json:"workflow_id"`
+	BranchDest               string                     `json:"branch_dest"`
+	PullRequestID            string                     `json:"pull_request_id"`
+	PullRequestRepositoryURL string                     `json:"pull_request_repository_url"`
+	PullRequestMergeBranch   string                     `json:"pull_request_merge_branch"`
+	PullRequestHeadBranch    string                     `json:"pull_request_head_branch"`
+	Environments             []EnvironmentVariableModel `json:"environments"`
+	BranchDestRepoOwner      string                     `json:"branch_dest_repo_owner"`
+	BranchRepoOwner          string                     `json:"branch_repo_owner"`
+}
+
+// EnvironmentVariableModel ...
+type EnvironmentVariableModel struct {
+	MappedTo string `json:"mapped_to"`
+	Value    string `json:"value"`
+	IsExpand bool   `json:"is_expand"`
+}
+
 func failf(format string, v ...interface{}) {
 	log.Errorf(format, v...)
 	os.Exit(1)
 }
 
 func rebuildAPICall() (string, error) {
-	buildParamToEnvVar := map[string]string{
-		"workflow_id":                 "BITRISE_TRIGGERED_WORKFLOW_ID",
-		"commit_hash":                 "BITRISE_GIT_COMMIT",
-		"branch":                      "BITRISE_GIT_BRANCH",
-		"branch_dest":                 "BITRISEIO_GIT_BRANCH_DEST",
-		"pull_request_id":             "PULL_REQUEST_ID",
-		"pull_request_repository_url": "BITRISEIO_PULL_REQUEST_REPOSITORY_URL",
-		"pull_request_merge_branch":   "BITRISEIO_PULL_REQUEST_MERGE_BRANCH",
-	}
-	buildParamToContents := map[string]string{
-		"BYPASS_GATE":    "true",
-		"commit_message": "Rebuilding with manually accepted step-info change.",
-	}
-	for buildParam, envName := range buildParamToEnvVar {
-		buildParamToContents[buildParam] = os.Getenv(strings.ToUpper(envName))
+	buildParams := BuildParamsModel{
+		Branch:                   os.Getenv("BITRISE_GIT_BRANCH"),
+		Tag:                      os.Getenv("BITRISE_GIT_TAG"),
+		CommitHash:               os.Getenv("BITRISE_GIT_COMMIT"),
+		CommitMessage:            "Rebuilding with manually accepted step-info change.",
+		WorkflowID:               os.Getenv("BITRISE_TRIGGERED_WORKFLOW_ID"),
+		BranchDest:               os.Getenv("BITRISEIO_GIT_BRANCH_DEST"),
+		PullRequestID:            os.Getenv("PULL_REQUEST_ID"),
+		PullRequestRepositoryURL: os.Getenv("BITRISEIO_PULL_REQUEST_REPOSITORY_URL"),
+		PullRequestMergeBranch:   os.Getenv("BITRISEIO_PULL_REQUEST_MERGE_BRANCH"),
+		PullRequestHeadBranch:    os.Getenv("BITRISEIO_PULL_REQUEST_HEAD_BRANCH"),
+		Environments: []EnvironmentVariableModel{
+			EnvironmentVariableModel{
+				MappedTo: "BYPASS_GATE",
+				Value:    "true",
+			},
+		},
 	}
 
-	buildParams, err := json.Marshal(buildParamToContents)
+	buildParamsBytes, err := json.Marshal(buildParams)
 	if err != nil {
-		return "", fmt.Errorf("rebuildAPICall: failed to marshal build params, envVars: %s, %v", buildParamToEnvVar, err)
+		return "", fmt.Errorf("rebuildAPICall: failed to marshal build params, buildParams: %+v, %v", buildParams, err)
 	}
 
 	return fmt.Sprintf(`$ curl https://app.bitrise.io/app/a0bac497f75e1490/build/start.json --data '{"hook_info":{"type":"bitrise","build_trigger_token":" < insert_build_trigger_token > "},
 "build_params": %s,
-"triggered_by":"curl"}'`, string(buildParams)), nil
+"triggered_by":"curl"}'`, string(buildParamsBytes)), nil
 }
 
 func main() {
