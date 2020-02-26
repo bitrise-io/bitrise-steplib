e919eecc0e5c623859096d52253b91bc5e63b629
Tamas Papik <trapacska@gmail.com>
2020-02-26 15:33:35 +0100

    test updated

--- handlers/validate/validators/collection/collection_test.go
+++ handlers/validate/validators/collection/collection_test.go
@@ -23,23 +23,13 @@ func Test_isUnsupportedVersion(t *testing.T) {
 			want:                false,
 		},
 		{
-			name:                "one supported version",
-			unsupportedVersions: []string{"3.2.1"},
+			name:                "2 unsupported version + version NOT included",
+			unsupportedVersions: []string{"3.2.1", "1.2.1"},
 			want:                false,
 		},
 		{
-			name:                "multiple supported versions",
-			unsupportedVersions: []string{"3.2.1", "4.2.1", "1.2.1"},
-			want:                false,
-		},
-		{
-			name:                "one unsupported version",
-			unsupportedVersions: []string{"1.2.3"},
-			want:                true,
-		},
-		{
-			name:                "one unsupported version from multiple versions",
-			unsupportedVersions: []string{"3.2.1", "1.2.3", "1.2.1"},
+			name:                "2 unsupported version + version included",
+			unsupportedVersions: []string{"3.2.1", "1.2.3"},
 			want:                true,
 		},
 	}
