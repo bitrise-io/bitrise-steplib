c51c68bf8bc8af00b550489062fc2efc6005ed33
Tamas Papik <trapacska@gmail.com>
2020-02-26 14:04:02 +0100

    cleanup

--- handlers/validate/validators/collection/collection_test.go
+++ handlers/validate/validators/collection/collection_test.go
@@ -14,44 +14,38 @@ func Test_isUnsupportedVersion(t *testing.T) {
 
 	tests := []struct {
 		name                string
-		v                   *version.Version
 		unsupportedVersions []string
 		want                bool
 	}{
 		{
 			name:                "no unsupported versions",
-			v:                   v,
 			unsupportedVersions: nil,
 			want:                false,
 		},
 		{
 			name:                "one supported version",
-			v:                   v,
 			unsupportedVersions: []string{"3.2.1"},
 			want:                false,
 		},
 		{
 			name:                "multiple supported versions",
-			v:                   v,
 			unsupportedVersions: []string{"3.2.1", "4.2.1", "1.2.1"},
 			want:                false,
 		},
 		{
 			name:                "one unsupported version",
-			v:                   v,
 			unsupportedVersions: []string{"1.2.3"},
 			want:                true,
 		},
 		{
 			name:                "one unsupported version from multiple versions",
-			v:                   v,
 			unsupportedVersions: []string{"3.2.1", "1.2.3", "1.2.1"},
 			want:                true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := isUnsupportedVersion(tt.v, tt.unsupportedVersions...); got != tt.want {
+			if got := isUnsupportedVersion(v, tt.unsupportedVersions...); got != tt.want {
 				t.Errorf("isUnsupportedVersion() = %v, want %v", got, tt.want)
 			}
 		})
